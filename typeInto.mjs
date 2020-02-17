1bb4117e5b9d4c6861c6f1fcd55b2cb35aa465e8
rockyshiba <leejsitu@gmail.com>
2020-02-18 00:16:54 +0900

    type into util typeInto.mjs

---
+++ typeInto.mjs
@@ -0,0 +1,37 @@
+import puppeteer from "puppeteer";
+
+(async () => {
+    try {
+        // setup
+        const browser = await puppeteer.launch({
+            slowMo: 500,
+            headless: false,
+        });
+
+        const page = await browser.newPage();
+        await page.goto("https://www.google.com");
+
+        typeSelectorEnter(
+            page,
+            "input.gLFyf.gsfi",
+            "cats"
+        );
+    } catch(err) {
+        console.log(err);
+    }
+})();
+
+/**
+ * Simulates a user selecting a selector, clicking 3 times into it to select all text, then typing to replace that text, then pressing Enter
+ * @param {object} page     page object initated by puppeteer
+ * @param {string} selector CSS selector, usually an input
+ * @param {string} text     text for the selector
+ */
+async function typeSelectorEnter(page, selector, text) {
+    const input = await page.$(selector);
+    await input.click({ clickCount: 3});
+    await input.type(text);
+
+    // press enter
+    await page.keyboard.press("Enter");
+}
